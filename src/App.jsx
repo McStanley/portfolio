6351bd7152bf806a2fa5ab4f0ca9900a3a4ebc0a
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-07-27 20:16:46 +0200

    Create footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div className="App-backgroundShape" />
       <Header />
       <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import Envelope from '../assets/envelope.svg';
+import GithubLogo from '../assets/github.svg';
+import TwitterLogo from '../assets/twitter.svg';
+import ImageMobile from '../assets/footer-image-mobile.jpg';
+import ImageTablet from '../assets/footer-image-tablet.jpg';
+import ImageDesktop from '../assets/footer-image-desktop.jpg';
+import './styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <div className="Footer-content">
+        <section className="Footer-info">
+          <h2>Contact me</h2>
+          <p className="Footer-text">
+            Please get in touch if you think our work could be mutually
+            beneficial!
+          </p>
+          <div className="Footer-emailContainer">
+            <img className="Footer-envelope" src={Envelope} alt="Email" />
+            <a
+              className="Footer-email"
+              href="mailto:0stanislawolejniczak0@gmail.com"
+            >
+              0stanislawolejniczak0@gmail.com
+            </a>
+          </div>
+          <div className="Footer-links">
+            <a
+              href="https://github.com/McStanley"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="Footer-logo" src={GithubLogo} alt="Github" />
+            </a>
+            <a
+              href="https://twitter.com/16stanley16"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="Footer-logo" src={TwitterLogo} alt="Twitter" />
+            </a>
+          </div>
+        </section>
+        <picture className="Footer-image">
+          <source media="(min-width: 1200px)" srcSet={ImageDesktop} />
+          <source media="(min-width: 600px)" srcSet={ImageTablet} />
+          <img src={ImageMobile} alt="" />
+        </picture>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
